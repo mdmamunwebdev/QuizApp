6c6c3794c91ccadfd5780a0d9a265fa9d9c78d21
mdmamunwebdev <76407019+mdmamunwebdev@users.noreply.github.com>
2022-03-05 02:02:57 +0600

    some updated in this project

--- js/questions.js
+++ js/questions.js
@@ -44,8 +44,63 @@ let questions = [
     ]
   },
   {
-    position: 'last',
     numb: 5,
+    question: "What does CSS stand for?",
+    answer: "D . Cascading Style Sheet",
+    options: [
+      "A . Common Style Sheet",
+      "B . Colorful Style Sheet",
+      "C . Computer Style Sheet",
+      "D . Cascading Style Sheet"
+    ]
+  },
+  {
+    numb: 6,
+    question: "What does HTML stand for?",
+    answer: "B . Hyper Text Markup Language",
+    options: [
+      "A . Hyper Text Preprocessor",
+      "B . Hyper Text Markup Language",
+      "C . Hyper Text Multiple Language",
+      "D . Hyper Tool Multi Language"
+    ]
+  },
+  {
+    numb: 7,
+    question: "What does CSS stand for?",
+    answer: "D . Cascading Style Sheet",
+    options: [
+      "A . Common Style Sheet",
+      "B . Colorful Style Sheet",
+      "C . Computer Style Sheet",
+      "D. Cascading Style Sheet"
+    ]
+  },
+  {
+    numb: 8,
+    question: "What does PHP stand for?",
+    answer: "A . Hypertext Preprocessor",
+    options: [
+      "A . Hypertext Preprocessor",
+      "B . Hypertext Programming",
+      "C . Hypertext Preprogramming",
+      "D . Hometext Preprocessor"
+    ]
+  },
+  {
+    numb: 9,
+    question: "What does SQL stand for?",
+    answer: "D . Structured Query Language",
+    options: [
+      "A . Stylish Question Language",
+      "B . Stylesheet Query Language",
+      "C . Statement Question Language",
+      "D . Structured Query Language"
+    ]
+  },
+  {
+    position: 'last',
+    numb: 10,
     question: "What does CSS stand for?",
     answer: "D . Cascading Style Sheet",
     options: [
