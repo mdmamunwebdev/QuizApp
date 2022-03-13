f81f493cd1ff640de62d692adbe93b281981e1b6
mdmamunwebdev <76407019+mdmamunwebdev@users.noreply.github.com>
2022-03-13 23:44:31 +0600

    ..

--- js/questions.js
+++ js/questions.js
@@ -1,26 +1,123 @@
-// creating an array and passing the number, questions, options, and answers
 let questions = [
-    {
-    numb: 1,
-    question: "What does HTML stand for?",
-    answer: "B . Hyper Text Markup Language",
-    options: [
-      "A . Hyper Text Preprocessor",
-      "B . Hyper Text Markup Language",
-      "C . Hyper Text Multiple Language",
-      "D . Hyper Tool Multi Language"
-    ]
-  },
-    {
-    position: 'last',  
-    numb: 2,
-    question: "What does CSS stand for?",
-    answer: "D . Cascading Style Sheet",
-    options: [
-      "A . Common Style Sheet",
-      "B . Colorful Style Sheet",
-      "C . Computer Style Sheet",
-      "D . Cascading Style Sheet"
-    ]
-  }
+	{
+		numb: 1,
+		question: "Maja hat viele Freunde, aber nicht alle sind ein Insekt. Welche dieser Tiere sind Insekten?",
+		sc_answer: "C",
+		answer: "Grashüpfer wie Flip, denn Insekten haben sechs Beine.",
+		additional: "Nicht alles kleine, krabbelige, mit vielen Beinen ist ein Insekt. Insekten haben insgesamt sechs Beine. Aber aufgepasst: Bei manchen Faltern sind die beiden vorderen Beine zu sogenannten Putzpfoten umgewandelt, sodass es aussieht, als hätten sie nur vier Beine. Manche Leute denken, Spinnen sind auch Insekten. Das stimmt aber nicht. Spinnen haben acht Beine und gehören zur Art der Spinnentiere. Regenwürmer wie Max haben überhaupt keine Beine und sind deshalb auch keine Insekten. Flip hat in der Serie zwei Beine und vier Arme. In der Wirklichkeit haben Grashüpfer natürlich keine Arme, sondern insgesamt sechs Beine und ist damit ein Insektenfreund von Maja. <br><br>Mehr über Majas wilde Freunde erfahrt ihr im Menü unter Warum sind Insekten wichtig?",
+		options: [
+			"Regenwürmer wie Max, denn Insekten haben keine Beine.",
+			"Spinnen wie Thekla, denn Insekten haben acht Beine.",
+			"Grashüpfer wie Flip, denn Insekten haben sechs Beine."
+		]
+	},
+	{
+		numb: 2,
+		question: "Was machen Bienen?",
+		sc_answer: "A",
+		answer: "Arbeitsbienen sammeln Pollen und Nektar und produzieren daraus Honig.",
+		additional: "In der Serie spielt Maja den ganzen Tag auf der Klatschmohnwiese. Die Biene Willi dagegen ist ein kleiner Faulpelz, der den ganzen Tag schläft. In der echten Welt sind Bienen aber sehr fleißig. Sie fliegen von Blüte zu Blüte. Dort sammeln Bienen Pollen und Nektar ein, den eine Pflanze hergestellt hat und produzieren daraus Honig. Für ein Kilogramm Honig fliegen die fleißigen Bienchen zu ungefähr zehn Millionen Blüten! <br><br>Mehr über die Aufgabe der Bienen lernt ihr im Menü unter Was machen Bienen?",
+		options: [
+			"Arbeitsbienen sammeln Pollen und Nektar und produzieren daraus Honig.",
+			"Sie spielen den ganzen Tag auf der Klatschmohnwiese, so wie Maja.",
+			"Bienen sind faul, sie schlafen die meiste Zeit ihres Lebens, so wie Willi."
+		]
+	},
+	{
+		numb: 3,
+		question: "Wie stark sind Ameisen wie Freddy und Eddy?",
+		sc_answer: "A",
+		answer: "Ameisen können das 50- bis 100-fache ihres eigenen Gewichtes tragen.",
+		additional: "Ameisen sind so stark, dass sie viel, viel mehr als die Hälfte ihres eigenen Gewichtes tragen können - nämlich das 50- bis 100-fache! Ganz so stark, dass sie das 500-fache tragen könnten, sind sie aber nicht. <br><br>Mehr Interessantes über Insekten findet ihr im Menü unter Überraschende Insekten-Fakten.",
+		options: [
+			"Ameisen können das 50- bis 100-fache ihres eigenen Gewichtes tragen.",
+			"Sie können das 500-fache ihres eigenen Gewichtes tragen.",
+			"Ameisen können die Hälfte ihres eigenen Gewichtes tragen."
+		]
+	},
+	{
+		numb: 4,
+		question: "Warum rollen Mistkäfer wie Ben fast immer eine Mistkugel?",
+		sc_answer: "A",
+		answer: "Mistkäfer versorgen ihre Larven mit dem Kot pflanzenfressender Säugetiere und halten so die Wiese sauber und die Erde fruchtbar.",
+		additional: "Mistkäfer und ihre Mistkugeln sind für die Natur wichtig. Wie der Name schon verrät, fressen Mistkäfer hauptsächlich Dung, also Kot von pflanzenfressenden Säugetieren wie Kühen und Pferden. Ihre Nahrung rollen die Käfer schnellstmöglich zu ihrem unterirdischen Bau, um sie vor räuberischen Artgenossen zu beschützen und ihren Nachwuchs zu ernähren. Indem sie den Dung unter die Erde bringen, sorgen Mistkäfer dafür, dass die Erde fruchtbarer wird. <br><br>Mehr über Majas wilde Insektenfreunde erfahrt ihr im Menü unter Warum sind Insekten wichtig?",
+		options: [
+			"Mistkäfer versorgen ihre Larven mit dem Kot pflanzenfressender Säugetiere und halten so die Wiese sauber und die Erde fruchtbar.",
+			"Mistkäfer gehen gern Kegeln und haben dafür immer eine Kugel dabei.",
+			"Mistkäfer verstecken sich hinter ihren Mistkugeln vor Angreifern, weil sie richtige Feiglinge sind."
+		]
+	},
+	{
+		numb: 5,
+		question: "Nicht alle blühenden Pflanzen sind gleich hilfreich für Bienen wie Maja und Wanda. Aber warum nur?",
+		sc_answer: "C",
+		answer: "So wie Mama und Papa beim Einkaufen fliegen Bienen spezialisiert zu sogenannten “ungefüllten” Blüten, die es hier gibt und die das anbieten, was die Biene gerade braucht: Nektar, Pollen oder beides.",
+		additional: "Bienen fliegen spezialisiert bestimmte Blüten an, da nicht alle blühenden Pflanzen gleich viel Pollen und Nektar produzieren. Einige Blüten bieten sogar nur entweder Pollen oder Nektar an. Beides ist für die Bienen von großer Bedeutung. Der Pollen wird als eiweißhaltige Nahrung für den Nachwuchs benötigt, der Nektar ist Energiequelle für die ausgewachsene Biene und wird als Honig als Energievorrat für den Winter eingelagert. <br><br>Tipps, Informationen und eine übersichtliche Liste mit bienenfreundlichen Pflanzen findet ihr im Menü unter Pflanztipps.",
+		options: [
+			"Pflanzen wie Lavendel duften so gut, dass Bienen vergessen, ihrer Arbeit nachzugehen. Süchtig nach dem Geruch fliegen sie immer wieder um die gleiche Blume. Deshalb sollte man weniger stark duftende Pflanzen einpflanzen.",
+			"Am besten für die Bienen sind Pflanzen mit gleichen Blütenfarben. Ist der Garten zu bunt, verlieren die Bienen nämlich ihre Orientierung und finden nicht mehr zurück zum Bienenstock oder Nest.",
+			"So wie Mama und Papa beim Einkaufen fliegen Bienen spezialisiert zu sogenannten “ungefüllten” Blüten, die es hier gibt und die das anbieten, was die Biene gerade braucht: Nektar, Pollen oder beides."
+		]
+	},
+	{
+		numb: 6,
+		question: "Wie nennt man männliche Bienen wie Willi?",
+		sc_answer: "B",
+		answer: "Drohnen",
+		additional: "Ein Bienenvolk besteht fast nur aus Weibchen: Die Arbeiterinnen sammeln Futter und pflegen die Brut und die Bienenkönigin legt die Eier und steuert den Staat. Männliche Bienen heißen Drohnen und sorgen zusammen mit der Königin für den Nachwuchs im Bienenstock.<br><br>Mehr über verschiedene Bienen und ihre Rollen lernt ihr im Menü unter Bienen in Serie.",
+		options: [
+			"Arbeiter",
+			"Drohnen",
+			"Bienenköniginnen"
+		]
+	},
+	{
+		numb: 7,
+		question: "Ihr wisst sicher, dass Marienkäfer wie Lara Punkte haben. Aber wisst ihr auch, was man an den Punkten erkennen kann?",
+		sc_answer: "B",
+		answer: "Man erkennt, zu welcher Käferart der Marienkäfer gehört.",
+		additional: "Viele Menschen denken, dass man anhand der Punkte das Alter eines Marienkäfers erkennt. Das stimmt aber nicht. Marienkäfer haben nämlich ihr ganzes Leben lang gleich viele Punkte. Die Punkte beschreiben eigentlich, zu welcher Käferart der Marienkäfer gehört. So wie euer Nachname verrät, zu welcher Familie ihr gehört. In Deutschland kommt zum Beispiel der <i>Siebenpunkt-Marienkäfer</i> häufig vor. Auf seinen Flügeln könnt ihr sieben Punkte zählen. Der <i>Veränderliche Flach-Marienkäfer</i> ist weniger häufig in Deutschland und hat eigentlich neun Punkte. Aber aufgepasst: Die vorderen kleinen Punkte sind nicht immer sichtbar!<br><br>Mehr Interessantes über Insekten findet ihr im Menü unter Überraschende Insekten-Fakten.",
+		options: [
+			"Man erkennt, wie alt ein Marienkäfer ist.",
+			"Man erkennt, zu welcher Käferart der Marienkäfer gehört.",
+			"Man erkennt, wie viel der Marienkäfer heute gegessen hat."
+		]
+	},
+	{
+		numb: 8,
+		question: "Schmetterlinge wie Beatrix haben wunderschöne, bunte Flügel – aber warum nur?",
+		sc_answer: "B",
+		answer: "Die bunten Flügel sollen Feinde davor warnen, dass sie ungenießbar sind.",
+		additional: "Obwohl den Schmetterlingen ihre wunderschönen Farben bestimmt gut gefallen und sie liebend gern von Blumen naschen, sind beide Antworten falsch. Viele Schmetterlinge haben bunte Flügel, um ihre Feinde (zum Beispiel Vögel, Fledermäuse oder Spinnen) davor zu warnen, dass sie ungenießbar oder giftig sind. Es gibt tatsächlich giftige Schmetterlingsarten. Der Großteil der Schmetterlinge täuscht ihre Giftigkeit aber nur vor. Es gibt auch Schmetterlinge, die Tarnfarben (zum Beispiel Braun) tragen. Das ist ein anderer Trick der Schmetterlinge, mit dem sie ihren Feinden entkommen.<br><br>Mehr über Majas wilde Insektenfreunde erfahrt ihr im Menü unter Warum sind Insekten wichtig?",
+		options: [
+			"Jeder Schmetterling trägt die Farben, die ihm am besten gefallen.",
+			"Die bunten Flügel sollen Feinde davor warnen, dass sie ungenießbar sind.",
+			"Schmetterlinge haben bunte Flügel, weil sie so viel Blumennektar fressen. Irgendwann nehmen ihre Flügel die Farbe der Blumen an, die sie oft fressen."
+		]
+	},
+	{
+		numb: 9,
+		question: "Warum reiben Fliegen wie Walter ihre Beine immer zu aneinander?",
+		sc_answer: "A",
+		answer: "Fliegen haben kleine Haare an ihren Beinen, mit denen sie schmecken. Sie halten deshalb ihre Beine sehr sauber, indem sie die Beine aneinander reiben.",
+		additional: "Fliegen sind keine Bösewichte, die die Weltherrschaft an sich reißen wollen. Im Gegenteil – Walters Artgenossen sind sehr wichtig für die Natur, denn sie sind ein wesentliches Glied in der Nahrungskette: Zum Beispiel Frösche, Wespen und Spinnen ernähren sich von Fliegen. Stubenfliegen zersetzen außerdem Kot und Kompost, sodass Pflanzen auf einem fruchtbaren Boden wachsen. Aber warum reiben Fliegen nun die Beine aneinander? Fliegen schmecken mit ihren Beinen. Sie halten deshalb ihre Beine sehr sauber, indem sie die Beinpaare mehrmals pro Minute aneinander reiben. <br><br>Mehr Interessantes über Insekten findet ihr im Menü unter Überraschende Insekten-Fakten. ",
+		options: [
+			"Fliegen haben kleine Haare an ihren Beinen, mit denen sie schmecken. Sie halten deshalb ihre Beine sehr sauber, indem sie die Beine aneinander reiben.",
+			"Fliegen reiben ihre Beine aneinander, um sich warm zu halten.",
+			"Sie hecken insgeheim einen Plan aus, um die Weltherrschaft an sich zu reißen."
+		]
+	},
+	{
+		position: 'last',
+		numb: 10,
+		question: "Wie könnt ihr Majas Insektenfreunden helfen?",
+		sc_answer: "C",
+		answer: "Ihr bastelt Nisthilfen oder Insektenhotels als Zuhause für die Insekten.",
+		additional: "Wiesen und Gärten sind für Insekten wichtig, denn dort leben sie und finden Nahrung. Viele mähen ihren Rasen zu früh, zu häufig oder zu viel. Für eine Wiese voller Leben sollte man am besten erst ab Mitte Juni in Abständen von zwei bis drei Wochen mähen und mindestens eine Rasenhöhe von zwölf Zentimeter stehen lassen. Auch auf Pflanzenschutzmittel solltet ihr verzichten, denn sie sind giftig für Pflanzen und Insekten. Wenn es weniger Pflanzen gibt, finden Insekten keinen Ort zum Nisten und nichts zum Fressen. Deshalb könnt ihr den Insekten einen riesigen Gefallen tun, indem ihr Nisthilfen und Insektenhotels bastelt und so einen neuen Lebensraum in eurem Garten oder auf eurem Balkon schafft.",
+		options: [
+			"Ihr verwendet Pflanzenschutzmittel, damit die Blumen gut wachsen.",
+			"Ihr mäht einmal die Woche den Rasen, damit Insekten darauf Golf spielen können.",
+			"Ihr bastelt Nisthilfen oder Insektenhotels als Zuhause für die Insekten."
+		]
+	}
 ];
